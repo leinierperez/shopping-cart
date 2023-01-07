d077c48e119f22f29c94bf8368246e7506878655
Leinier Perez <P_leinier@yahoo.com>
2023-01-07 04:53:02 -0500

    Add `Product` component and display the products in the shop route

---
+++ src/components/Product.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function Product({ name, price, image, id }) {
+  return (
+    <Link
+      to={id.toString()}
+      className="max-w-[16rem] cursor-pointer space-y-2 overflow-hidden rounded-sm text-gray-100"
+    >
+      <img className="" src={image} alt={`${name} Image`} />
+      <h2 className="text-2xl font-semibold">{name}</h2>
+      <p className="text-xl">$ {price}</p>
+    </Link>
+  );
+}
+
+export default Product;
--- src/routes/Shop.jsx
+++ src/routes/Shop.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { products } from '../products/products';
+import Product from '../components/Product';
 
 function Shop() {
-  return <div>SHOP PAGE</div>;
+  return (
+    <main className="mx-auto grid max-w-screen-xl grid-cols-[repeat(auto-fit,minmax(theme(width.12),theme(width.60)))] justify-center gap-10 py-14 px-4 ">
+      {products.map(({ name, id, price, image }) => (
+        <Product name={name} key={id} price={price} image={image} id={id} />
+      ))}
+    </main>
+  );
 }
 
 export default Shop;
