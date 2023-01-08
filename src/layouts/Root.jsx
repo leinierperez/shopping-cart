39e7383dcd4f0491dc79ac95f659b65404fc64b6
Leinier Perez <P_leinier@yahoo.com>
2023-01-07 22:01:01 -0500

    Create product info page and display cart size in header

--- src/layouts/Root.jsx
+++ src/layouts/Root.jsx
@@ -1,7 +1,12 @@
+import { useContext } from 'react';
 import { Outlet } from 'react-router-dom';
 import { Link } from 'react-router-dom';
+import CartContext from '../CartContext';
 
 export default function Root() {
+  const { cart } = useContext(CartContext);
+  const cartSize = [...new Set(cart.map((product) => product.name))].length;
+
   return (
     <div className="text-white">
       <header className="bg-gray-900 text-2xl font-semibold">
@@ -14,22 +19,7 @@ export default function Root() {
           <div className="flex items-center gap-8">
             <Link to="/">Home</Link>
             <Link to="shop">Shop</Link>
-            <Link to="cart">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="h-7 w-7"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M2.25 3h1.386c.51 0 .955.343 1.087.835l.383 1.437M7.5 14.25a3 3 0 00-3 3h15.75m-12.75-3h11.218c1.121-2.3 2.1-4.684 2.924-7.138a60.114 60.114 0 00-16.536-1.84M7.5 14.25L5.106 5.272M6 20.25a.75.75 0 11-1.5 0 .75.75 0 011.5 0zm12.75 0a.75.75 0 11-1.5 0 .75.75 0 011.5 0z"
-                />
-              </svg>
-            </Link>
+            <Link to="cart">Cart ({cartSize})</Link>
           </div>
         </nav>
       </header>
--- src/routes/ProductInfo.jsx
+++ src/routes/ProductInfo.jsx
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useContext } from 'react';
+import { useCallback } from 'react';
+import { useParams } from 'react-router-dom';
+import { products } from '../products/products';
+import CartContext from '../CartContext';
 
 function ProductInfo() {
-  return <div>PRODUCT INFO PAGE</div>;
+  const { id } = useParams();
+  const { setCart } = useContext(CartContext);
+  const { name, description, price, image } = useCallback(
+    products.find((product) => product.id == id)
+  );
+  const addToCart = () => {
+    setCart((prevCart) => [...prevCart, { name, description, price, image }]);
+  };
+
+  return (
+    <div>
+      <img className="" src={image} alt={`${name} Image`} />
+      <h2 className="text-2xl font-semibold">{name}</h2>
+      <p className="text-xl">$ {price}</p>
+      <p className="">$ {description}</p>
+      <button className="bg-slate-900" onClick={addToCart}>
+        Add to cart
+      </button>
+    </div>
+  );
 }
 
 export default ProductInfo;
