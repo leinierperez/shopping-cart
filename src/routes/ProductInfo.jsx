f1c46ab3e6489868b1cdab08ee970026cff64023
Leinier Perez <P_leinier@yahoo.com>
2023-01-11 11:23:16 -0500

    Style the product info page

--- src/routes/ProductInfo.jsx
+++ src/routes/ProductInfo.jsx
@@ -13,19 +13,21 @@ function ProductInfo() {
   );
 
   return (
-    <div>
-      <img className="" src={image} alt={`${name} Image`} />
-      <h2 className="text-2xl font-semibold">{name}</h2>
-      <p className="text-xl">$ {price}</p>
-      <p className="">$ {description}</p>
-      <button
-        className="bg-slate-900"
-        onClick={() =>
-          addToCart({ name, id, description, price, image, count: 1 })
-        }
-      >
-        Add to cart
-      </button>
+    <div className="mt-14 flex flex-col px-4 md:flex-row">
+      <img className="max-w-md rounded-md" src={image} alt={`${name} Image`} />
+      <div className="md: mt-2 flex flex-col gap-4 px-4">
+        <h2 className="text-2xl font-semibold">{name}</h2>
+        <p className="text-xl">${price}</p>
+        <p className="text-lg">{description}</p>
+        <button
+          className="self-start rounded-full bg-slate-900 py-2 px-6 text-lg"
+          onClick={() =>
+            addToCart({ name, id, description, price, image, count: 1 })
+          }
+        >
+          Add to cart
+        </button>
+      </div>
     </div>
   );
 }
